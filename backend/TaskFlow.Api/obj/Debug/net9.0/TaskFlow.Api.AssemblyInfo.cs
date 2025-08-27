025fa19549028ccd4531907feefcb6aef67cee7c
Barak1998 <barakm25@gmail.com>
2025-08-27 19:15:52 +0300

    chore: update assembly informational version and cache for TaskFlow.Api
    - Updated the AssemblyInformationalVersion in TaskFlow.Api.AssemblyInfo.cs for consistency.
    - Modified the TaskFlow.Api.AssemblyInfoInputs.cache to reflect changes in the assembly inputs.

--- backend/TaskFlow.Api/obj/Debug/net9.0/TaskFlow.Api.AssemblyInfo.cs
+++ backend/TaskFlow.Api/obj/Debug/net9.0/TaskFlow.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskFlow.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb853b86522fdb2c143feafbb4350650cac981e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb04c24d792a968ccbb118f42fd7fda246c3d275")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskFlow.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskFlow.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
